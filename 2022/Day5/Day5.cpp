6fdb35b3879ed377542fad410919eddfa22c6408
knuffelturk <aran1997.ay@gmail.com>
2023-07-02 16:56:17 +0200

    Day5

---
+++ 2022/Day5/Day5.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+std::string cargoArray[9];
+void ExecuteMoveInstructions(int, int, int);
+
+int main()
+{
+	std::ifstream input("Input.txt");
+	bool countingCargo = true;
+	int moveInstructions[3];
+	
+
+	if (input.is_open()) {
+		std::string line;
+
+		while (std::getline(input, line)) {
+			if (countingCargo) {
+				if (line.empty()) {
+					countingCargo = false;
+				}
+				else {
+					for (int i = 0; i < 9; i++) {
+						char currentBox = line[i * 4 + 1];
+						if (currentBox == '1')break;
+						if (currentBox != ' ') {
+							cargoArray[i] += currentBox;
+						}
+					}
+				}
+			}
+			else {		//countingCargo == false
+				for (int i = 0; i < 3; i++) {
+					int it = line.find(' ');
+					line.erase(0, it + 1);
+					it = line.find(' ');
+					moveInstructions[i] = std::stoi(line.substr(0, it));
+					line.erase(0, it + 1);
+				}
+				ExecuteMoveInstructions(moveInstructions[0], moveInstructions[1], moveInstructions[2]);
+			}
+		}
+		input.close();
+		std::string answer;
+		for (int i = 0; i < 9; i++) {
+			answer += cargoArray[i][0];
+		}
+		std::cout << "answer: " << answer << std::endl;
+	}
+
+	return 0;
+}
+
+
+void ExecuteMoveInstructions(int moveAmount, int moveStartLoc, int moveTowards) {
+	for (int i = 0; i < moveAmount; i++) {
+		char box = cargoArray[moveStartLoc - 1][0];
+		cargoArray[moveTowards - 1].insert(0, 1, box);
+		cargoArray[moveStartLoc - 1].erase(0, 1);
+	}
+}
